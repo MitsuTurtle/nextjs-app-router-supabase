9bba8b6eee722c073e20b035d9c9c048c2240915
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-01-06 18:10:20 +0900

    Data fetch in server component

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import NotesList from './components/notes-list'
+
+export default function Page() {
+  return (
+    <main>
+      <div className="m-10 text-center">Hello World🚀</div>
+      <NotesList />
+    </main>
+  )
+}
